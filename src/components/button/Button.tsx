f019d4abded67494c2ab6cdc75140be89557e909
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-10-16 20:18:22 -0300

    Configured tailwind.js and storybook

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,16 @@
+/*
+import React, {FunctionComponent} from "react";
+
+const buttonVariant = cva()
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<ButtonProps> {
+    icon?: FunctionComponent<any>
+}
+
+export default function Button(props: ButtonProps) {
+    return(<button className={buttonVariant({size, variant})}>
+        {children}
+    </button>);
+}
+
+*/

5a992fe7f340b20077d431724f475e20a3e38596
Fede Pochat <federico.pochat@ing.austral.edu.ar>
2024-10-19 14:26:02 -0300

    Text input

---
+++ src/components/text-input/TextInput.stories.tsx
@@ -0,0 +1,42 @@
+import {Meta, StoryObj} from "@storybook/react";
+import {TextInput} from "./TextInput";
+
+const meta: Meta<typeof TextInput> = {
+    title: 'Components/TextInput',
+    component: TextInput,
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {
+        color: {
+            control: "select",
+            options: ["primary", "danger", "gray"]
+        },
+        disabled: {
+            control: "boolean"
+        },
+        rounded: {
+            control: "boolean"
+        }
+    },
+};
+export default meta;
+
+type Story = StoryObj<typeof TextInput>;
+
+export const Default: Story = {
+    args: {
+        label: 'Username',
+        color: "primary",
+        placeholder: "Username...",
+    },
+};
+
+export const Disabled: Story = {
+    args: {
+        label: 'Disabled',
+        disabled: true,
+        color: "gray",
+        value: "readonly",
+    },
+};
---
+++ src/components/text-input/TextInput.tsx
@@ -0,0 +1,42 @@
+import {InputHTMLAttributes} from "react";
+import {cva, VariantProps} from "class-variance-authority";
+
+const textInputVariant = cva("flex flex-row gap-2 p-2 bg-gray-100", {
+    variants: {
+        color: {
+            primary: "focus:outline-primary",
+            danger: "focus:outline-danger",
+            gray: "focus:outline-gray-200",
+        },
+        disabled: {
+            true: "cursor-not-allowed text-gray-500",
+            false: "",
+        },
+        rounded: {
+            true: "rounded",
+            false: ""
+        },
+    },
+    defaultVariants: {
+        disabled: false,
+        rounded: true
+    },
+});
+
+
+export interface TextInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "color" | "size">, Omit<VariantProps<typeof textInputVariant>, "disabled"> {
+    label?: string;
+}
+
+export const TextInput = ({
+                              label,
+                              color,
+                              ...props
+                          }: TextInputProps) => {
+    return (
+        <div>
+            {label && <label>{label}</label>}
+            <input className={textInputVariant({disabled: props.disabled, color})} {...props} />
+        </div>
+    );
+}

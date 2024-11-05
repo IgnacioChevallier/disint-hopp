1b79502f460c39e82ac199844cef79f7169befbf
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-11-05 19:25:34 -0300

    Updated ListItem.tsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import {Button} from "./components/button/Button";
 import {ListItem} from "./components/list-item/ListItem";
+import {TextInput} from "./components/text-input/TextInput";
 
 function App() {
     return (
         <div className="App">
             <Button text={"Click me"}/>
-            <ListItem text={"Boca"}/>
+            <div style={{width: '20%', gap: 5, display: 'flex', flexDirection: 'column'}}>
+                <ListItem text={"Boca"} controlType={"radio"}/>
+                <ListItem text={"Juniors"} controlType={"toggle"}/>
+                <ListItem text={"Juega"} controlType={"checkbox"}/>
+            </div>
+            <TextInput placeholder={"Enter your name"}/>
         </div>
     );
 }

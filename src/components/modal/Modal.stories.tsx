901ca68c9537c811cf7b7886ac8a356ec9b30aad
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-11-05 19:25:29 -0300

    added checked and unchecked versions

--- src/components/modal/Modal.stories.tsx
+++ src/components/modal/Modal.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import {Modal} from "./Modal";
 import Icon from "../icon/Icon";
+import {CheckBox} from "../checkbox/CheckBox";
 
 const meta: Meta<typeof Modal> = {
     title: 'Components/Modal',
@@ -35,3 +36,47 @@ export const Default: Story = {
         ]
     }
 };
+
+export const CheckModal: Story = {
+    args: {
+        rows: [
+            {
+                left: <CheckBox selected={true} />,
+                label: "Option 1",
+                right: ""
+            },
+            {
+                left: <CheckBox selected={true} />,
+                label: "Option 2",
+                right: ""
+            },
+            {
+                left: <CheckBox selected={true} />,
+                label: "Option 3",
+                right: ""
+            }
+        ]
+    }
+}
+
+export const UncheckModal: Story = {
+    args: {
+        rows: [
+            {
+                left: <CheckBox selected={false} />,
+                label: "Option 1",
+                right: ""
+            },
+            {
+                left: <CheckBox selected={false} />,
+                label: "Option 2",
+                right: ""
+            },
+            {
+                left: <CheckBox selected={false} />,
+                label: "Option 3",
+                right: ""
+            }
+        ]
+    }
+}

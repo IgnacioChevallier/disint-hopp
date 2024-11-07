1a816fb43eb04325ffc5614b44fa4db4252f8b93
matias <mchialva@mail.austral.edu.ar>
2024-11-06 23:58:11 -0300

    FamilyGroupHeader -> storybook.

---
+++ src/components/family-group-header/FamilyGroupHeader.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import FamilyGroupHeader from './FamilyGroupHeader';
+import { iconMap } from '../icon/IconMap';
+
+const metaFamilyGroupHeader: Meta<typeof FamilyGroupHeader> = {
+    title: 'Components/FamilyGroupHeader',
+    component: FamilyGroupHeader,
+    parameters: {
+        layout: 'top',
+    },
+    argTypes: {
+        iconName: {
+            control: 'select',
+            options: Array.from(iconMap.keys()),
+        },
+        title: {
+            control: 'text',
+        },
+    },
+};
+
+export default metaFamilyGroupHeader;
+
+export const Default = {
+    args: {
+        title: 'Family Group',
+        iconName: 'arrow back',
+        iconName2: 'account circle',
+    },
+};
--- src/components/family-group-header/FamilyGroupHeader.tsx
+++ src/components/family-group-header/FamilyGroupHeader.tsx
@@ -4,25 +4,23 @@ import ProfilePicture from '../profile-picture/ProfilePicture';
 import Icon from '../icon/Icon';
 import {iconMap} from "../icon/IconMap";
 
-const familyGroupHeaderStyles = cva('flex items-center gap-2', {
+const familyGroupHeaderStyles = cva('flex items-center gap-2 text-black', {
     variants: {
-        type: {
-            default: 'text-black',
-        },
+        type: {},
     },
 });
-
 interface FamilyGroupHeaderProps extends VariantProps<typeof familyGroupHeaderStyles> {
     iconName: keyof typeof iconMap;
     title: string;
+    iconName2: keyof typeof iconMap;
 }
 
-const FamilyGroupHeader = ({ type, title, iconName }: FamilyGroupHeaderProps) => {
+const FamilyGroupHeader = ({ type, title, iconName, iconName2 }: FamilyGroupHeaderProps) => {
     return (
         <div className={familyGroupHeaderStyles({ type })}>
             <ProfilePicture iconName={iconName} size="medium" />
             <h2 className="text-h2-bold">{title}</h2>
-            <Icon name="location" size={'medium'} color="#0095FF" />
+            <Icon name={iconName2} size="medium"/>
         </div>
     );
 };

288c92596f10e6481b372bf2981786cf7f239a58
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-11-08 23:45:48 -0300

    DestinationPreview

---
+++ src/components/destination-preview/DestinationPreview.tsx
@@ -0,0 +1,20 @@
+import {Button} from "../button/Button";
+
+export interface DestinationPreviewProps {
+    destination: string,
+    finalStop: string,
+}
+
+export const DestinationPreview = ({destination, finalStop}: DestinationPreviewProps) => {
+    return (
+        <div className={"min-w-[300px] min-h-[184px] bg-gray-100 rounded-[16px] py-[16px]"}>
+            <div className={"flex flex-col justify-center items-center"}>
+                <h3 className={"text-h3-bold"}>{destination}</h3>
+                <h1 className={"text-h1-bold"}>{finalStop}</h1>
+            </div>
+            <span className={"flex justify-center items-center mt-[24px]"}>
+                <Button text={"View Map"} variant={"contained"} size={"small"} trailingIcon={"location"} trailingIconColor={"white"} />
+            </span>
+        </div>
+    )
+}

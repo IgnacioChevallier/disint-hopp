3398cd21994fe451982cbd648193de88c88a85aa
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-11-12 11:05:58 -0300

    SlideModalBox

---
+++ src/components/slide-modal-box/SlideModalBox.tsx
@@ -0,0 +1,16 @@
+import {ReactNode} from "react";
+
+export interface SlideModalBoxProps {
+    children: ReactNode
+}
+
+export const SlideModalBox = ({children}: SlideModalBoxProps) => {
+    return(
+        <div className={"min-w-[395px] max-w-[395px] rounded-t-[16px] bg-background-main p-[16px] shadow-[rgba(0,0,15,0.5)_0px_-2px_3px_-1px]"}>
+            <div className={"min-w-[64px] max-w-[64px] min-h-[4px] rounded-full bg-gray-500 relative top-0 inset-0 m-auto"}/>
+            <div className={"mt-[10px]"}>
+                {children}
+            </div>
+        </div>
+    )
+}

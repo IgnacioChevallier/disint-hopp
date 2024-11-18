3e85398a1cc522813ce152da162c41017101e967
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-11-18 20:44:20 -0300

    update FamilyMemberInfo

--- src/components/family-member-info/FamilyMemberInfo.tsx
+++ src/components/family-member-info/FamilyMemberInfo.tsx
@@ -3,7 +3,7 @@ import { cva, VariantProps } from 'class-variance-authority';
 import ProfilePicture from '../profile-picture/ProfilePicture';
 import { IconButton } from "../icon-button/IconButton";
 
-const familyMemberInfoStyles = cva('flex items-center gap-3 p-3 rounded-lg bg-gray-100 w-full justify-between', {
+const familyMemberInfoStyles = cva('flex items-center gap-3 p-[8px] rounded-[8px] bg-white w-full justify-between', {
     variants: {
         type: {},
     },
@@ -18,14 +18,12 @@ interface FamilyMemberInfoProps extends VariantProps<typeof familyMemberInfoStyl
 const FamilyMemberInfo = ({ type, link, userName, role }: FamilyMemberInfoProps) => {
     return (
         <div className={familyMemberInfoStyles({ type })}>
-            <div className="flex items-center gap-3 overflow-hidden">
-                <ProfilePicture link={link} size="small" />
-                <div className="flex flex-col overflow-hidden">
-                    <h2 className="text-h3-bold truncate">{userName}</h2>
-                    <span className="text-p-regular text-primary truncate">{role}</span>
-                </div>
+            <div className="flex overflow-hidden justify-center items-center gap-[16px]">
+                <ProfilePicture link={link} size="small"/>
+                <h3 className="text-h3-regular text-gray-600 truncate">{userName}</h3>
+                <span className="text-p-regular text-primary truncate">{role}</span>
             </div>
-            <IconButton iconName="three dots" size="medium" color="gray" />
+            <IconButton iconName="three dots" size="medium" color="gray"/>
         </div>
     );
 };

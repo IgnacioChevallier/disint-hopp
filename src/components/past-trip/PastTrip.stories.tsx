829f5a75352d8cd095a790ff53007bd74c6c43e8
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-11-23 14:29:24 -0300

    PastTrip

---
+++ src/components/past-trip/PastTrip.stories.tsx
@@ -0,0 +1,64 @@
+import {Meta, StoryObj} from "@storybook/react";
+import {iconMap} from "../icon/IconMap";
+import {PastTrip} from "./PastTrip";
+
+
+const meta: Meta<typeof PastTrip> = {
+    title: 'Components/PastTrip',
+    component: PastTrip,
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {
+        mainIcon: {
+            control: "select",
+            options: [undefined, ...Array.from(iconMap.keys())]
+        },
+        start: {
+            control: "text"
+        },
+        end: {
+            control: "text"
+        },
+        price: {
+            control: "text",
+        },
+        time: {
+            control: "text"
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof PastTrip>;
+
+export const Home: Story = {
+    args: {
+        mainIcon: 'home',
+        start: 'School',
+        end: 'Home',
+        price: '$1300',
+        time: '20/11/2024 15:24',
+    },
+};
+
+export const Work: Story = {
+    args: {
+        mainIcon: 'store',
+        start: 'Home',
+        end: 'Store',
+        price: '$1500',
+        time: '19/11/2024 15:32',
+    },
+};
+
+export const School: Story = {
+    args: {
+        mainIcon: 'school',
+        start: 'Home',
+        end: 'School',
+        price: '$1300',
+        time: '15/11/2024 09:02',
+    },
+};

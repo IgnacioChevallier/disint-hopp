6a6e081439981ca557aa1904762bd0ad64462bff
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-11-23 12:39:36 -0300

    update GoogleLoginButton

--- src/components/google-login-button/GoogleLoginButton.tsx
+++ src/components/google-login-button/GoogleLoginButton.tsx
@@ -3,7 +3,7 @@ import Icon from "../icon/Icon";
 
 export const GoogleLoginButton = ({...props}:ButtonHTMLAttributes<HTMLButtonElement>) => {
     return (
-        <button className={"min-w-[300px] min-h-[46px] bg-gray-100 rounded-[21px] pl-[20px] gap-[10px] text-h3-regular text-gray-600 flex flex-row items-center"} {...props}>
+        <button className={"min-w-[300px] min-h-[46px] bg-white border-[1px] border-gray-200 rounded-[21px] pl-[20px] gap-[10px] text-h3-regular text-gray-600 flex flex-row items-center"} {...props}>
             <Icon name={'google'} size={"medium"}/>
             Continue with Google
         </button>

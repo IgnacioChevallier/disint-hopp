274b481a50d863e939651b786eca7cd9d55262ce
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-11-25 19:31:47 -0300

    added header for notifications

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,29 +11,33 @@ const familyGroupHeaderStyles = cva('flex items-center text-black justify-betwee
 });
 
 interface HeaderProps extends VariantProps<typeof familyGroupHeaderStyles> {
-    trailingIconName: keyof typeof iconMap;
+    trailingIconName: string;
     title: string;
-    leadingIconName: keyof typeof iconMap;
+    leadingIconName: string;
+    trailingOnClick?: () => void;
+    leadingOnClick?: () => void;
     showSearchbar?: boolean;
     options?: string[];
     placeholder?: string;
 }
 
-const Header = ({ type, title, trailingIconName, leadingIconName, showSearchbar = false, placeholder, options }: HeaderProps) => {
+const Header = ({ type, title, trailingIconName, leadingIconName, showSearchbar = false, placeholder, options , leadingOnClick, trailingOnClick}: HeaderProps) => {
     const [isSearchbarVisible, setSearchbarVisible] = useState(showSearchbar);
 
     return (
-        <div>
+        <div className={"w-full"}>
             <div>
                 <div className={familyGroupHeaderStyles({ type }) + " bg-gray-100"}>
-                    <IconButton iconName={leadingIconName} size="medium" />
+                    <IconButton iconName={leadingIconName} size="medium" onClick={leadingOnClick} />
                     <h2 className="text-h2-bold line-clamp-1">{title}</h2>
-                    <IconButton iconName={trailingIconName} size='medium' />
+                    <IconButton iconName={trailingIconName} size='medium' onClick={trailingOnClick} />
                 </div>
             </div>
+            {isSearchbarVisible && placeholder && options &&
             <div className="pl-12 pr-12 pb-4 bg-gray-100">
-                {isSearchbarVisible && placeholder && options && <Searchbar placeholder={placeholder} options={options} trailingIcon={"search"} />}
+                <Searchbar placeholder={placeholder} options={options} trailingIcon={"search"} />
             </div>
+            }
             <div className="w-full h-0.5 bg-gray-300"></div>
         </div>
     );
--- src/screens/Notifications.tsx
+++ src/screens/Notifications.tsx
@@ -7,6 +7,7 @@ import { CheckBox } from "../components/checkbox/CheckBox";
 import { IconButton } from "../components/icon-button/IconButton";
 import { Sidebar } from "../components/sidebar/Sidebar";
 import { SidebarElement } from "../components/sidebar-element/SidebarElement";
+import Header from "../components/header/Header";
 
 const Notifications = () => {
     const alerts = [
@@ -59,197 +60,192 @@ const Notifications = () => {
     };
 
     return (
-        <div className="p-4 flex flex-col bg-background-main text-black min-h-screen gap-y-3 relative">
-            {/*{isSidebarOpen && (*/}
-            {/*    <div*/}
-            {/*        className="fixed inset-0 bg-black bg-opacity-50 z-40"*/}
-            {/*        onClick={() => setIsSidebarOpen(false)}*/}
-            {/*    />*/}
-            {/*)}*/}
-
-            {/*{isSidebarOpen && (*/}
-            {/*    <div*/}
-            {/*        ref={sidebarRef}*/}
-            {/*        className="absolute left-0 top-0 z-50"*/}
-            {/*    >*/}
-            {/*        <Sidebar>*/}
-            {/*            <SidebarElement*/}
-            {/*                selected={selectedElement === 0}*/}
-            {/*                icon={"search"}*/}
-            {/*                text={"Search"}*/}
-            {/*                onClick={() => setSelectedElement(0)}*/}
-            {/*            />*/}
-            {/*            <SidebarElement*/}
-            {/*                selected={selectedElement === 1}*/}
-            {/*                icon={"groups"}*/}
-            {/*                text={"Family Group"}*/}
-            {/*                onClick={() => setSelectedElement(1)}*/}
-            {/*            />*/}
-            {/*            <SidebarElement*/}
-            {/*                selected={selectedElement === 2}*/}
-            {/*                icon={"history"}*/}
-            {/*                text={"Trip History"}*/}
-            {/*                onClick={() => setSelectedElement(2)}*/}
-            {/*            />*/}
-            {/*            <SidebarElement*/}
-            {/*                selected={selectedElement === 3}*/}
-            {/*                icon={"bus alert"}*/}
-            {/*                text={"Alerts"}*/}
-            {/*                onClick={() => setSelectedElement(3)}*/}
-            {/*            />*/}
-            {/*            <SidebarElement*/}
-            {/*                selected={selectedElement === 4}*/}
-            {/*                icon={"account circle"}*/}
-            {/*                text={"Account"}*/}
-            {/*                onClick={() => setSelectedElement(4)}*/}
-            {/*            />*/}
-            {/*        </Sidebar>*/}
-            {/*    </div>*/}
-            {/*)}*/}
-
-
-            {/*FIXME: Rompe las imagenes de las alertas y no toco nada de ese codigo*/}
-
-            {isSidebarOpen && (
-                <div
-                    className="fixed inset-0 bg-black bg-opacity-50 z-40"
-                    onClick={() => setIsSidebarOpen(false)}
+        <div>
+            <div>
+                <Header
+                    trailingIconName="empty"
+                    title={"Alerts"}
+                    leadingIconName="menu"
+                    leadingOnClick={handleSidebarToggle}
+                    showSearchbar={true}
+                    options={alerts}
+                    placeholder={"Search"}
                 />
-            )}
+            </div>
+            <div className="p-4 flex flex-col bg-background-main text-black min-h-screen gap-y-3 relative">
+                {/*{isSidebarOpen && (*/}
+                {/*    <div*/}
+                {/*        className="fixed inset-0 bg-black bg-opacity-50 z-40"*/}
+                {/*        onClick={() => setIsSidebarOpen(false)}*/}
+                {/*    />*/}
+                {/*)}*/}
 
-            <div
-                ref={sidebarRef}
-                className={`
+                {/*{isSidebarOpen && (*/}
+                {/*    <div*/}
+                {/*        ref={sidebarRef}*/}
+                {/*        className="absolute left-0 top-0 z-50"*/}
+                {/*    >*/}
+                {/*        <Sidebar>*/}
+                {/*            <SidebarElement*/}
+                {/*                selected={selectedElement === 0}*/}
+                {/*                icon={"search"}*/}
+                {/*                text={"Search"}*/}
+                {/*                onClick={() => setSelectedElement(0)}*/}
+                {/*            />*/}
+                {/*            <SidebarElement*/}
+                {/*                selected={selectedElement === 1}*/}
+                {/*                icon={"groups"}*/}
+                {/*                text={"Family Group"}*/}
+                {/*                onClick={() => setSelectedElement(1)}*/}
+                {/*            />*/}
+                {/*            <SidebarElement*/}
+                {/*                selected={selectedElement === 2}*/}
+                {/*                icon={"history"}*/}
+                {/*                text={"Trip History"}*/}
+                {/*                onClick={() => setSelectedElement(2)}*/}
+                {/*            />*/}
+                {/*            <SidebarElement*/}
+                {/*                selected={selectedElement === 3}*/}
+                {/*                icon={"bus alert"}*/}
+                {/*                text={"Alerts"}*/}
+                {/*                onClick={() => setSelectedElement(3)}*/}
+                {/*            />*/}
+                {/*            <SidebarElement*/}
+                {/*                selected={selectedElement === 4}*/}
+                {/*                icon={"account circle"}*/}
+                {/*                text={"Account"}*/}
+                {/*                onClick={() => setSelectedElement(4)}*/}
+                {/*            />*/}
+                {/*        </Sidebar>*/}
+                {/*    </div>*/}
+                {/*)}*/}
+
+
+                {/*FIXME: Rompe las imagenes de las alertas y no toco nada de ese codigo*/}
+
+                {isSidebarOpen && (
+                    <div
+                        className="fixed inset-0 bg-black bg-opacity-50 z-40"
+                        onClick={() => setIsSidebarOpen(false)}
+                    />
+                )}
+
+                <div
+                    ref={sidebarRef}
+                    className={`
                     fixed top-0 left-0 h-full z-50 bg-background-main w-64 
                     transform transition-transform duration-300 
                     ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}
                 `}
-            >
-                <Sidebar>
-                    <SidebarElement
-                        selected={selectedElement === 0}
-                        icon={"search"}
-                        text={"Search"}
-                        onClick={() => setSelectedElement(0)}
-                    />
-                    <SidebarElement
-                        selected={selectedElement === 1}
-                        icon={"groups"}
-                        text={"Family Group"}
-                        onClick={() => setSelectedElement(1)}
-                    />
-                    <SidebarElement
-                        selected={selectedElement === 2}
-                        icon={"history"}
-                        text={"Trip History"}
-                        onClick={() => setSelectedElement(2)}
-                    />
-                    <SidebarElement
-                        selected={selectedElement === 3}
-                        icon={"bus alert"}
-                        text={"Alerts"}
-                        onClick={() => setSelectedElement(3)}
-                    />
-                    <SidebarElement
-                        selected={selectedElement === 4}
-                        icon={"account circle"}
-                        text={"Account"}
-                        onClick={() => setSelectedElement(4)}
-                    />
-                </Sidebar>
-            </div>
+                >
+                    <Sidebar>
+                        <SidebarElement
+                            selected={selectedElement === 0}
+                            icon={"search"}
+                            text={"Search"}
+                            onClick={() => setSelectedElement(0)}
+                        />
+                        <SidebarElement
+                            selected={selectedElement === 1}
+                            icon={"groups"}
+                            text={"Family Group"}
+                            onClick={() => setSelectedElement(1)}
+                        />
+                        <SidebarElement
+                            selected={selectedElement === 2}
+                            icon={"history"}
+                            text={"Trip History"}
+                            onClick={() => setSelectedElement(2)}
+                        />
+                        <SidebarElement
+                            selected={selectedElement === 3}
+                            icon={"bus alert"}
+                            text={"Alerts"}
+                            onClick={() => setSelectedElement(3)}
+                        />
+                        <SidebarElement
+                            selected={selectedElement === 4}
+                            icon={"account circle"}
+                            text={"Account"}
+                            onClick={() => setSelectedElement(4)}
+                        />
+                    </Sidebar>
+                </div>
 
-            <div className="flex items-center justify-center relative">
-                <IconButton
-                    iconName="sidebar"
-                    size="small"
-                    className="absolute left-0"
-                    onClick={handleSidebarToggle}
-                />
-                <span className="text-h2-regular">Alerts</span>
-            </div>
 
-            <div className="w-full">
-                <Searchbar
-                    placeholder="Search..."
-                    leadingIcon=""
-                    trailingIcon="search"
-                    options={alerts}
-                />
-            </div>
+                <div className="flex justify-end">
+                    <Dropdown
+                        overlayProps={{
+                            rows: [
+                                {
+                                    label: "All",
+                                    right: <Toggle selected={isAllSelected} onClick={handleAllToggleClick} />,
+                                },
+                                {
+                                    label: "Favorites",
+                                    right: <CheckBox
+                                        selected={isFavoritesSelected}
+                                        onClick={handleFavoritesToggleClick}
+                                        size={"md"}
+                                    />,
+                                },
+                                {
+                                    label: "Ongoing",
+                                    right: <CheckBox
+                                        selected={isOngoingSelected}
+                                        onClick={handleOngoingToggleClick}
+                                        size={"md"}
+                                    />,
+                                },
+                            ],
+                        }}
+                        buttonProps={{
+                            text: "Filter",
+                            color: "primary",
+                            variant: "outlined",
+                            trailingIcon: "arrow down",
+                            rounded: "full",
+                            size: "medium",
+                            disableHover: true,
+                        }}
+                        overlayAlignment={"right"}
+                    />
+                </div>
 
-            <div className="flex justify-end">
-                <Dropdown
-                    overlayProps={{
-                        rows: [
-                            {
-                                label: "All",
-                                right: <Toggle selected={isAllSelected} onClick={handleAllToggleClick} />,
-                            },
-                            {
-                                label: "Favorites",
-                                right: <CheckBox
-                                    selected={isFavoritesSelected}
-                                    onClick={handleFavoritesToggleClick}
-                                    size={"md"}
-                                />,
-                            },
-                            {
-                                label: "Ongoing",
-                                right: <CheckBox
-                                    selected={isOngoingSelected}
-                                    onClick={handleOngoingToggleClick}
-                                    size={"md"}
-                                />,
-                            },
-                        ],
-                    }}
-                    buttonProps={{
-                        text: "Filter",
-                        color: "primary",
-                        variant: "outlined",
-                        trailingIcon: "arrow down",
-                        rounded: "full",
-                        size: "medium",
-                        disableHover: true,
-                    }}
-                    overlayAlignment={"right"}
-                />
-            </div>
-
-            <div className="flex flex-col gap-2 w-full">
-                <Alert
-                    active={true}
-                    link={false}
-                    href=""
-                    text="Lines 123, 434, 843, 947 currently affected by service issues."
-                    time="6:24 am"
-                    icon="bus alert"
-                />
-                <Alert
-                    active={true}
-                    link={false}
-                    href=""
-                    text="High passenger volume on Line 843 due to local events."
-                    time="6:30 am"
-                    icon="bus alert"
-                />
-                <Alert
-                    active={false}
-                    link={false}
-                    href=""
-                    text="Unexpected incident on Line 123. Delays expected."
-                    time="8:00 am"
-                    icon="calendar clock"
-                />
-                <Alert
-                    active={false}
-                    link={false}
-                    href=""
-                    text="Line 123 will be temporarily unavailable from 10 pm to 5 am."
-                    time="9:00 pm"
-                    icon="calendar clock"
-                />
+                <div className="flex flex-col gap-2 w-full">
+                    <Alert
+                        active={true}
+                        link={false}
+                        href=""
+                        text="Lines 123, 434, 843, 947 currently affected by service issues."
+                        time="6:24 am"
+                        icon="bus alert"
+                    />
+                    <Alert
+                        active={true}
+                        link={false}
+                        href=""
+                        text="High passenger volume on Line 843 due to local events."
+                        time="6:30 am"
+                        icon="bus alert"
+                    />
+                    <Alert
+                        active={false}
+                        link={false}
+                        href=""
+                        text="Unexpected incident on Line 123. Delays expected."
+                        time="8:00 am"
+                        icon="calendar clock"
+                    />
+                    <Alert
+                        active={false}
+                        link={false}
+                        href=""
+                        text="Line 123 will be temporarily unavailable from 10 pm to 5 am."
+                        time="9:00 pm"
+                        icon="calendar clock"
+                    />
+                </div>
             </div>
         </div>
     );

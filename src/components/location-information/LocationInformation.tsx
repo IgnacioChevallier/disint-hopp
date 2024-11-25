7f0db8ad8f22cfdf7f0986e3f742f535f34a17f1
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2024-11-25 18:39:05 -0300

    added the location information

--- src/components/location-information/LocationInformation.tsx
+++ src/components/location-information/LocationInformation.tsx
@@ -1,17 +1,22 @@
 import {cva} from "class-variance-authority";
 import {LocationImage, LocationImageProps} from "../location-image/LocationImage";
 
-export const locationInformation = cva("flex flex-col gap-[10px] max-w-[390px] min-w-[390px] z-10 w-full" +
+export const locationInformation = cva("flex flex-col gap-[10px] w-full z-10 w-full" +
     " rounded-t-[16px] bg-background-main p-[16px] shadow-[rgba(0,0,15,0.5)_0px_-2px_3px_-1px]");
 
 interface LocationInformationProps {
     name: string;
     images: LocationImageProps[];
+    positionAtBottom?: boolean;
 }
 
-export const LocationInformation = ({name, images}: LocationInformationProps) => {
+export const LocationInformation = ({name, images, positionAtBottom = false}: LocationInformationProps) => {
     return (
-        <div className={locationInformation()}>
+        <div
+            className={`${locationInformation()} ${
+                positionAtBottom ? "absolute bottom-0 left-0 w-full z-50" : ""
+            }`}
+        >
             <span className="text-h3-extra-bold">
                 {name}
             </span>
--- src/components/trip-summary-list/TripSummaryList.tsx
+++ src/components/trip-summary-list/TripSummaryList.tsx
@@ -1,13 +1,13 @@
 import {TripSummaryComponent} from "../trip-summary/TripSummary";
 import React from "react";
 
-export interface TripSummaryListProps {
+export interface TripSummaryListProps extends React.HTMLProps<HTMLDivElement>{
     children: TripSummaryComponent | TripSummaryComponent[]
 }
 
-export const TripSummaryList = ({ children }: TripSummaryListProps) => {
+export const TripSummaryList = ({ children, ...props }: TripSummaryListProps) => {
     return (
-        <div className={"flex flex-col gap-[8px] py-[8px]"}>
+        <div className={"flex flex-col gap-[8px] py-[8px]"} {...props}>
             {children}
         </div>
     )

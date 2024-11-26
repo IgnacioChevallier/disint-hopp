8bcc0ee81535d39b586142b1eb228b7a74a3f8c3
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-11-26 12:11:42 -0300

    added Image carousel to RouteList page

--- src/screens/RouteList.tsx
+++ src/screens/RouteList.tsx
@@ -1,16 +1,16 @@
 import {Dropdown} from "../components/dropdown/Dropdown";
 import React, {useState} from "react";
 import {CheckBox} from "../components/checkbox/CheckBox";
-import {Searchbar} from "../components/searchbar/Searchbar";
-import {IconButton} from "../components/icon-button/IconButton";
 import {TripSummaryList} from "../components/trip-summary-list/TripSummaryList";
 import {TripSummary} from "../components/trip-summary/TripSummary";
 import {TransportationMethod} from "../components/transportation-method/TransportationMethod";
 import {useNavigate} from "react-router-dom";
 import Icon from "../components/icon/Icon";
 import {LocationInformation} from "../components/location-information/LocationInformation";
-import {LocationImageProps} from "../components/location-image/LocationImage";
 import RouteListHeader from "../components/route-list-header/RouteListHeader";
+import {LocationImageCarousel} from "../components/location-image-carousel/LocationImageCarousel";
+import {Sidebar} from "../components/sidebar/Sidebar";
+import {SidebarElement} from "../components/sidebar-element/SidebarElement";
 
 const RouteList = () => {
     const places = [
@@ -23,23 +23,25 @@ const RouteList = () => {
         "University of Michigan",
     ];
 
-    const images: LocationImageProps[] = [
-        {
-            src: "https://www.usnews.com/cmsmedia/5c/38cc5da3a6a0f690dd7f46438ae170/Above-Cobb-Gate.jpg",
-            alt: "University of Chicago",
-            size: "small"
-        },
-        {
-            src: "https://www.edvisehub.com/wp-content/uploads/2022/11/UChicagoUpdate_0912.jpg",
-            alt: "University of Chicago",
-            size: "small"
-        },
-        {
-            src: "https://www.chicagomag.com/wp-content/archive/images/2011/0311/C201103-U-of-C-campus.jpg",
-            alt: "University of Chicago",
-            size: "small"
-        }
-    ];
+    const getImages = (size: "small" | "medium" | "auto") => {
+        return [
+            {
+                src: "https://www.usnews.com/cmsmedia/5c/38cc5da3a6a0f690dd7f46438ae170/Above-Cobb-Gate.jpg",
+                alt: "University of Chicago",
+                size
+            },
+            {
+                src: "https://www.edvisehub.com/wp-content/uploads/2022/11/UChicagoUpdate_0912.jpg",
+                alt: "University of Chicago",
+                size
+            },
+            {
+                src: "https://www.chicagomag.com/wp-content/archive/images/2011/0311/C201103-U-of-C-campus.jpg",
+                alt: "University of Chicago",
+                size
+            }
+        ];
+    }
 
     const [locationValue, setLocationValue] = useState("Current Location");
     const [routeValue, setRouteValue] = useState("University of Chicago");
@@ -51,6 +53,8 @@ const RouteList = () => {
     const [isBusSelected, setIsBusSelected] = useState(false);
     const [isTrainSelected, setIsTrainSelected] = useState(false);
 
+    const [isCarouselOpen, setIsCarouselOpen] = useState<boolean>(false);
+    const [initialCarouselImage, setInitialCarouselImage] = useState<number>(0);
 
     const handleSubwayToggleClick = () =>{
         setIsSubwaySelected(!isSubwaySelected);
@@ -79,6 +83,11 @@ const RouteList = () => {
             places.filter(alert => alert.toLowerCase().includes(newValue.toLowerCase()))
         );
     };
+
+    const onImageClick = (index: number) => {
+        setInitialCarouselImage(index);
+        setIsCarouselOpen(true);
+    }
 
     return (
         <div className="flex flex-col h-full w-full bg-background-main">
@@ -133,29 +142,40 @@ const RouteList = () => {
                     />
                 </div>
             </div>
+            <div>
+                {isCarouselOpen && <LocationImageCarousel images={getImages("auto")} index={initialCarouselImage} onClose={() => {setIsCarouselOpen(false)}}/>}
+            </div>
+
             <div className="flex-grow overflow-y-auto">
                 <TripSummaryList className="pb-32 mb-8">
-                    <TripSummary time={"1:30pm - 2:10pm"} duration={"40min"} onClick={() => alert("selected this route")}>
+                    <TripSummary time={"1:30pm - 2:10pm"} duration={"40min"}
+                                 onClick={() => alert("selected this route")}>
                         <TransportationMethod icon={"directions bus"} text={"123"} duration={"15min"} color={"blue"}/>
                         <TransportationMethod icon={"directions bus"} text={"321"} duration={"15min"} color={"blue"}/>
                         <TransportationMethod icon={"directions bus"} text={"789"} duration={"5min"} color={"blue"}/>
                         <TransportationMethod icon={"directions bus"} text={"1000"} duration={"5min"} color={"blue"}/>
                     </TripSummary>
-                    <TripSummary time={"2:00pm - 2:45pm"} duration={"45min"} onClick={() => alert("selected this route")}>
+                    <TripSummary time={"2:00pm - 2:45pm"} duration={"45min"}
+                                 onClick={() => alert("selected this route")}>
                         <TransportationMethod icon={"directions bus"} text={"678"} duration={"15min"} color={"blue"}/>
-                        <TransportationMethod icon={"directions bus"} text={"123"} duration={"15min"} color={"darkBlue"}/>
+                        <TransportationMethod icon={"directions bus"} text={"123"} duration={"15min"}
+                                              color={"darkBlue"}/>
                         <TransportationMethod icon={"directions bus"} text={"900"} duration={"15min"} color={"pink"}/>
                     </TripSummary>
-                    <TripSummary time={"1:50pm - 2:50pm"} duration={"1hrs"} onClick={() => alert("selected this route")}>
+                    <TripSummary time={"1:50pm - 2:50pm"} duration={"1hrs"}
+                                 onClick={() => alert("selected this route")}>
                         <TransportationMethod icon={"directions bus"} text={"555"} duration={"15min"} color={"yellow"}/>
                         <TransportationMethod icon={"directions walk"} text={""} duration={"45min"} color={"gray"}/>
                     </TripSummary>
-                    <TripSummary time={"2:10pm - 2:55pm"} duration={"45min"} onClick={() => alert("selected this route")}>
+                    <TripSummary time={"2:10pm - 2:55pm"} duration={"45min"}
+                                 onClick={() => alert("selected this route")}>
                         <TransportationMethod icon={"directions bus"} text={"235"} duration={"15min"} color={"blue"}/>
                         <TransportationMethod icon={"directions walk"} text={""} duration={"15min"} color={"gray"}/>
-                        <TransportationMethod icon={"directions bus"} text={"563"} duration={"15min"} color={"darkBlue"}/>
+                        <TransportationMethod icon={"directions bus"} text={"563"} duration={"15min"}
+                                              color={"darkBlue"}/>
                     </TripSummary>
-                    <TripSummary time={"1:25pm - 9:25pm"} duration={"8hrs"} onClick={() => alert("selected this route")}>
+                    <TripSummary time={"1:25pm - 9:25pm"} duration={"8hrs"}
+                                 onClick={() => alert("selected this route")}>
                         <TransportationMethod icon={"directions walk"} text={""} duration={"30min"} color={"blue"}/>
                     </TripSummary>
                 </TripSummaryList>
@@ -163,8 +183,9 @@ const RouteList = () => {
             <div className="fixed bottom-0 left-0 right-0">
                 <LocationInformation
                     name="University of Chicago"
-                    images={images}
+                    images={getImages("small")}
                     positionAtBottom={true}
+                    onImageClick={onImageClick}
                 />
             </div>
         </div>
